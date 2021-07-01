70896229342f3fa7160e5f8a6ea9aa9e23aa8469
wanbaep <wanbaepp@naver.com>
2021-07-02 07:37:45 +0900

    Add flyway

---
+++ webapp/src/main/resources/db/migration/V1__Create_Membership_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE membership (
+     seq int,
+     membershipId varchar(255),
+     userId varchar(255),
+     membershipName varchar(255),
+     startDate datetime,
+     membershipStatus varchar(255),
+     point varchar(255)
+);
